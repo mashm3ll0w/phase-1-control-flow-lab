26a765e2a233b7c4b1a854279bcaaa9b80738631
Charles Swaleh <charlswall@live.com>
2022-11-29 08:52:26 +0300

    Updated code solution for control flow assignment

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 function scuberGreetingForFeet(ride){
   // Write your code here!
-  if (ride > 2500){
-    return "No can do."
-  }
-  else if (ride <= 400){
+  // if (ride > 2500){
+  //   return "No can do."
+  // }
+  // else if (ride <= 400){
+  //   return "This one is on me!";
+  // }
+  // else if (ride > 2000){
+  //   return "I will gladly take your thirty bucks.";
+  // }
+  
+  if (ride <= 400){
     return "This one is on me!";
   }
-  else if (ride > 2000){
-    return "I will gladly take your thirty bucks.";
+  else if(ride < 2500){
+    return "I will gladly take your thirty bucks."
   }
-  
+  else {
+    return "No can do."
+  }
 }
 
 function ternaryCheckCity(city){
@@ -17,18 +26,15 @@ function ternaryCheckCity(city){
   return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(amount){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-  switch (amount) {
+  switch (tip) {
     case "generous":
       return "Thank you so much."
-      break;
     case "not as generous":
       return "Thank you."
-      break;
     default:
       return "Bye.";
-      break;
   }
 }
 
