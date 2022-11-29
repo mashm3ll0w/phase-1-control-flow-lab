a243480685f6ec4dceb5ad6b14065d2eb3ae9d6a
Charles Swaleh <charlswall@live.com>
2022-11-29 08:51:11 +0300

    commit tree b9f8b5c21c00404ee459dbd6d475593fc08b5acb

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 function scuberGreetingForFeet(ride){
   // Write your code here!
-  if (ride > 2500){
-    return "No can do."
-  }
-  else if (ride <= 400){
+  // if (ride > 2500){
+  //   return "No can do."
+  // }
+  // else if (ride <= 400){
+  //   return "This one is on me!";
+  // }
+  // else if (ride > 2000){
+  //   return "I will gladly take your thirty bucks.";
+  // }
+  
+  if (ride <= 400){
     return "This one is on me!";
   }
-  else if (ride > 2000){
-    return "I will gladly take your thirty bucks.";
+  else if(ride < 2500){
+    return "I will gladly take your thirty bucks."
   }
-  
+  else {
+    return "No can do."
+  }
 }
 
 function ternaryCheckCity(city){
